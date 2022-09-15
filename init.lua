0ffc00fd92714219ad3076600ab8817c49ad9927
magokstad <magokstad@gmail.com>
2022-09-15 23:42:27 +0200

    added guifont

--- init.lua
+++ init.lua
@@ -1,5 +1,6 @@
 local autocmd = vim.api.nvim_create_autocmd
 
+vim.opt.guifont = "CodeNewRoman Nerd Font"
 -- Auto resize panes when resizing nvim window
 -- autocmd("VimResized", {
 --   pattern = "*",
