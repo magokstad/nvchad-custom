4c907fbbfe4fbcc851a1489f93acad7d704d3bc9
magokstad <magokstad@gmail.com>
2022-10-20 23:41:03 +0200

    fixed mappings, no more need for these

--- mappings.lua
+++ mappings.lua
@@ -4,13 +4,14 @@ local M = {}
 -- FIXME fix somethinbg
 M.general = {
   t = {
+    -- WARN: Not necessary anymore
     -- toggle in terminal mode
-    ["<C-i>"] = {
-      function()
-        require("nvterm.terminal").toggle "float"
-      end,
-      "toggle floating term",
-    },
+    -- ["<C-i>"] = {
+    --   function()
+    --     require("nvterm.terminal").toggle "float"
+    --   end,
+    --   "toggle floating term",
+    -- },
   },
 
   n = {
@@ -20,13 +21,14 @@ M.general = {
     ["<leader>ut"] = {"<cmd> :lua require('undotree').toggle() <CR>", "toggle undotree", opts={noremap = true, silent = true}},
 -- vim.keymap.set('n', '<leader>u', require('undotree').toggle, { noremap = true, silent = true })
 
+    -- WARN: Not necessary anymore
     -- toggle in normal mode
-    ["<C-i>"] = {
-      function()
-        require("nvterm.terminal").toggle "float"
-      end,
-      "toggle floating term",
-    },
+    -- ["<C-i>"] = {
+    --   function()
+    --     require("nvterm.terminal").toggle "float"
+    --   end,
+    --   "toggle floating term",
+    -- },
   },
 
 }
